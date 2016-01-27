48f8d4e4aa077ca80a056413979eb196390abb2e
du00 <duninglin@xiaomi.com>
2016-01-28 02:09:27 +0800

    parquet module & wikipedia test paruqet file

---
+++ extensions/parquet-extensions/src/test/scala/GenerateParquetFile.scala
@@ -0,0 +1,29 @@
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkContext, SparkConf}
+
+/**
+  * Copyright (c) 2015 XiaoMi Inc. All Rights Reserved.
+  * Authors: du00 <duninglin@xiaomi.com>
+  */
+
+/**
+  * Created by du00 on 16-1-28.
+  */
+object GenerateParquetFile extends App {
+  val conf = new SparkConf().setAppName("app").setMaster("local")
+  val sc = new SparkContext(conf)
+
+  val sqlContext = new SQLContext(sc)
+
+  val lines = sc.parallelize(Seq(
+    """{"timestamp": "2013-08-31T01:02:33Z", "page": "Gypsy Danger", "language" : "en", "user" : "nuclear", "unpatrolled" : "true", "newPage" : "true", "robot": "false", "anonymous": "false", "namespace":"article", "continent":"North America", "country":"United States", "region":"Bay Area", "city":"San Francisco", "added": 57, "deleted": 200, "delta": -143}""",
+    """{"timestamp": "2013-08-31T03:32:45Z", "page": "Striker Eureka", "language" : "en", "user" : "speed", "unpatrolled" : "false", "newPage" : "true", "robot": "true", "anonymous": "false", "namespace":"wikipedia", "continent":"Australia", "country":"Australia", "region":"Cantebury", "city":"Syndey", "added": 459, "deleted": 129, "delta": 330}""",
+    """{"timestamp": "2013-08-31T07:11:21Z", "page": "Cherno Alpha", "language" : "ru", "user" : "masterYi", "unpatrolled" : "false", "newPage" : "true", "robot": "true", "anonymous": "false", "namespace":"article", "continent":"Asia", "country":"Russia", "region":"Oblast", "city":"Moscow", "added": 123, "deleted": 12, "delta": 111}""",
+    """{"timestamp": "2013-08-31T11:58:39Z", "page": "Crimson Typhoon", "language" : "zh", "user" : "triplets", "unpatrolled" : "true", "newPage" : "false", "robot": "true", "anonymous": "false", "namespace":"wikipedia", "continent":"Asia", "country":"China", "region":"Shanxi", "city":"Taiyuan", "added": 905, "deleted": 5, "delta": 900}""",
+    """{"timestamp": "2013-08-31T12:41:27Z", "page": "Coyote Tango", "language" : "ja", "user" : "cancer", "unpatrolled" : "true", "newPage" : "false", "robot": "true", "anonymous": "false", "namespace":"wikipedia", "continent":"Asia", "country":"Japan", "region":"Kanto", "city":"Tokyo", "added": 1, "deleted": 10, "delta": -9}"""
+  ))
+
+  sqlContext.read.json(lines).foreach(println)
+
+  sc.stop()
+}
